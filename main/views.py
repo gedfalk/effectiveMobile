8b18a0a431a2024c0ed7b598a3bf8200a37d07f4
gedfalk <blite.co.stn@gmail.com>
2025-05-11 15:37:59 +0300

    feat: add item grid rendering on main page

--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from items.models import Item
 
 def index(request):
-    return render(request, 'main/index.html')
+    items = Item.objects.select_related('user_id', 'category').all().order_by('created_at')
+    context = {'items': items}
+    return render(request, 'main/index.html', context)
