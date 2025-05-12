57e900900d8fbe1ecea657226ed6c8ea30992095
gedfalk <blite.co.stn@gmail.com>
2025-05-12 16:23:20 +0300

    feat: styling add_item page with form widgets

--- items/forms.py
+++ items/forms.py
@@ -6,3 +6,21 @@ class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ['title', 'description', 'image', 'category', 'condition']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-input',
+                'placeholder': 'Введите название товара'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-textarea',
+                'placeholder': 'Введите описание товара...',
+                'rows': 3,
+            }),
+            'category': forms.Select(attrs={
+                'class': 'form-select',
+            }),
+            'condition': forms.Select(attrs={
+                'class': 'form-select',
+            })
+
+        }
